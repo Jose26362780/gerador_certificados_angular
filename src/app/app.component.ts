6584b38fe1b89a4a879703a4c761991d89b6d785
Jose Martinez <juniorjose1925@gmail.com>
2025-11-06 21:09:05 -0300

    add secondary button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component.spec';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  standalone: true,
+  imports: [NavbarComponent, PrimaryButtonComponent,SecondaryButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'gerador_certificados_angular';
