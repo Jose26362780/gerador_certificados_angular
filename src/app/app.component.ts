ab04da2c8146b27a95f28356f46e0bb0ce0b869a
Jose Martinez <juniorjose1925@gmail.com>
2025-11-13 08:12:32 -0300

    add routes of the aplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+
+
+
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
-import { CertificadosComponent } from "./pages/certificados/certificados.component";
-import { CertificadosFormComponent } from "./pages/certificado-form/certificado-form.component";
-import { CertificadoComponent } from './pages/certificado/certificado.component';
+
+
+
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent, CertificadosComponent, CertificadosFormComponent, CertificadoComponent],
+  imports: [NavbarComponent, BaseUiComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
+import { CertificadosFormComponent } from './pages/certificado-form/certificado-form.component';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:"",
+    component:CertificadosComponent,
+
+  },
+
+
+  {
+  path:"certificados/novo",
+  component:CertificadosFormComponent
+
+},
+
+{
+  path:"certificados/:id",
+  component: CertificadoComponent
+
+}
+
+];
