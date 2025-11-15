6481c0b9a608546fe49ba4e8b066f1801272d3de
Jose Martinez <juniorjose1925@gmail.com>
2025-11-15 13:22:27 -0300

    add ng model

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { FormsModule } from '@angular/forms';
+import { AngularJSUrlCodec } from '@angular/common/upgrade';
 
 @Component({
   selector: 'app-certificados-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent,FormsModule],
   templateUrl: './certificado-form.component.html',
   styleUrls: ['./certificado-form.component.css'],
 })
-export class CertificadosFormComponent {}
+export class CertificadosFormComponent {
+
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React'];
+}
